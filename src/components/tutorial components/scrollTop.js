eabbff0a427c66f83a3dbddf2c612a859e797ad6
Devin Lizardi <dlizardi@berkeley.edu>
2020-09-26 12:23:37 -0700

    Scroll button basic behaivor setup

--- src/components/tutorial components/scrollTop.js
+++ src/components/tutorial components/scrollTop.js
@@ -5,39 +5,41 @@ import { CSSTransition } from 'react-transition-group';
 function ScrollTop(props) {
     const breakpoint = 99;
     const [isEntered, setIsEntered] = useState(false);
-
-    var rollingDom;
-
+    const [enterClass, setEnterClass] = useState(" scrolled-line ");
+    
     let lastScrollPos = 0;
     let ticking = false;
 
     function handleScroll(scrollPos) {
         setIsEntered(scrollPos > breakpoint);
-        rollingDom.style.transform = isEntered ? "translateX(-160%)" : "translateX(0%)";
-        rollingDom.style.width = isEntered ? "3rem" : "5rem";
-        rollingDom.style.height = isEntered ? "1rem" : "0.75rem";
-        rollingDom.style.opacity = isEntered ? "0.3" : "1";
+        setEnterClass(isEntered ? " scroll-up-line " : " scrolled-line ");
+    }
+
+    function offSetHandleScroll() {
+        lastScrollPos = window.scrollY;
+        if (!ticking) {
+            window.requestAnimationFrame(() => {
+                handleScroll(lastScrollPos);
+                ticking = false;
+            });
+            ticking = true;
+        }
+    }
+
+    const scrollTop = () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+        handleScroll();
     }
 
     useEffect(() => {
-        rollingDom = document.getElementById("rollingline");
-        window.addEventListener("scroll", (e) => {
-            lastScrollPos = window.scrollY;
-
-            if (!ticking) {
-                window.requestAnimationFrame(() => {
-                    handleScroll(lastScrollPos);
-                    ticking = false;
-                });
-                ticking = true;
-            }
-        });
-        return () => {window.removeEventListener("scroll", handleScroll)};
+        window.addEventListener("scroll", offSetHandleScroll);
+        return () => {window.removeEventListener("scroll", offSetHandleScroll)};
     });
 
     return(
-        <div>
-        <span id="rollingline" className={props.sectionClass}/>
+        <div onClick={scrollTop}>
+        <span id="rollingline" className={props.sectionClass + enterClass}/>
         <ScrollUpArrow enter={isEntered}/>
         </div>
     );
@@ -47,7 +49,7 @@ function ScrollUpArrow(props) {
     const {enter} = props;
 
     return(
-        <CSSTransition in={enter} timeout={100} unmountOnExit>
+        <CSSTransition in={enter} timeout={100} unmountOnExit classNames="scroll-in">
             <svg className="scroll-svg" width="30" height="33" viewBox="0 0 23 26" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M1 18L11.5 8L23 18" stroke="black" strokeWidth="3"/>
             </svg>
