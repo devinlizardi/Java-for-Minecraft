d9d5e8b51cc91325f090968b4040667f1d298126
Devin Lizardi <dlizardi@berkeley.edu>
2020-10-04 12:21:17 -0700

    setting up q2

--- src/components/tutorial components/questions/question2a.js
+++ src/components/tutorial components/questions/question2a.js
@@ -15,18 +15,30 @@ function Question2A() {
                 <img src={LowerClass} className="rounded img-inst" id="class-lower"/>
             </div>
             <p>
-                <b>Note: </b> the <span className="inline-code"> Lower</span> class has a <em> function </em> called <span className="inline-code">
+                Note that the <span className="inline-code"> Lower</span> class has a <em> function </em> called <span className="inline-code">
                     riseUp()</span> that changes it's wealth, not the First's wealth
                 <br /><br />
                 Let's make some instances of these classes in the standard <span className="inline-code"> Main</span> class!
             </p>
             <img src={MainClass} className="rounded img-inst" id="class-main-ex"/>
-            <p>post text here</p>
+        {/* QUESTION 1 */}
+            <p>
+                <b>Question 1</b><br />
+                <span className="inline-code">bilbo</span> is an <span className="vocab-inst"> instance </span> of which class?
+            </p>
             <div className="answers">
-                <AnswerItem ans="answer title" value="incorrect">
-                dropdown text content
+                <AnswerItem ans="First" value="incorrect">
+                Not quite! Look at the <span className="inline-code">Type</span> of the variable when it's made on line 5.
+                </AnswerItem>
+                <AnswerItem ans="Main" value="incorrect">
+                While it's true that <span className="inline-code">Main</span> is a class it <em> could </em> be, it's not of this class.
+                </AnswerItem>
+                <AnswerItem ans="Lower" value="correct">
+                Perfect! On line 5 it's created as an instance of the Lower class. That might not be fair though, Bilbo was doing just
+                fine for himself before leaving the Shire.
                 </AnswerItem>
             </div>
+
         </div>
         </div>
     );
