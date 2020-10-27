0c00511eaf8b251440ffb23f40c1caabb7068d37
Devin Lizardi <Devin@Geetika-Lizardis-MacBook-Air-3.local>
2020-10-27 08:04:33 -0700

    small text addition

--- src/components/tutorial components/javaBasics03.js
+++ src/components/tutorial components/javaBasics03.js
@@ -14,7 +14,9 @@ function JavaBasics03(props) {
                     on the ideas we already have about classes and instances, and it's the last step we need 
                     to work through before starting coding for Minecraft.
                     <br /><br />
-                    Simply put: <span className="vocab-sub"> subclasses </span> are 
+                    Simply put <span className="vocab-sub"> subclasses </span> are extensions of a class; they fill
+                    in the values an abstract class declares. What makes them important to us is that in adding new things 
+                    to the game we have to start with and manipulate the base classes by extending them into subclasses.
                 </p>
             </div>
         </div>
